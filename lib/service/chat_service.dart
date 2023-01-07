6f18f18bf54ff302779a88ff2538dbe81651c304
Anton Kolesov <kolesov.anton.s@gmail.com>
2023-01-07 19:08:45 +0300

    Added simple implementation for chats page

---
+++ lib/service/chat_service.dart
@@ -0,0 +1,58 @@
+import 'package:intl/intl.dart';
+
+class Chat {
+  String avatarUrl;
+  String name;
+  String lastMessageText;
+  DateTime lastMessageDate;
+  int unread;
+
+  getLastMessageDateAsString(){
+    return DateFormat.MMMEd().format(lastMessageDate);
+  }
+
+  Chat(this.avatarUrl, this.name, this.lastMessageText, this.lastMessageDate,
+      this.unread);
+}
+
+abstract class AbstractChatService {
+  List<Chat> getChats();
+}
+
+class ChatService implements AbstractChatService {
+  @override
+  List<Chat> getChats() {
+    return [
+      Chat(
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoqWIPKg9kRQhn9r3qgpcRSACAXvg-dbTOWQiDN6b5ahLRZ-AU_ioL_uXv5Un0kNGPNhE&usqp=CAU',
+          'Anthony',
+          'What do you think?',
+          DateTime.now(),
+          2),
+      Chat(
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoqWIPKg9kRQhn9r3qgpcRSACAXvg-dbTOWQiDN6b5ahLRZ-AU_ioL_uXv5Un0kNGPNhE&usqp=CAU',
+          'Anthony',
+          'What do you think?',
+          DateTime.now(),
+          2),
+      Chat(
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoqWIPKg9kRQhn9r3qgpcRSACAXvg-dbTOWQiDN6b5ahLRZ-AU_ioL_uXv5Un0kNGPNhE&usqp=CAU',
+          'Anthony',
+          'What do you think?',
+          DateTime.now(),
+          2),
+      Chat(
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoqWIPKg9kRQhn9r3qgpcRSACAXvg-dbTOWQiDN6b5ahLRZ-AU_ioL_uXv5Un0kNGPNhE&usqp=CAU',
+          'Anthony',
+          'What do you think?',
+          DateTime.now(),
+          2),
+      Chat(
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoqWIPKg9kRQhn9r3qgpcRSACAXvg-dbTOWQiDN6b5ahLRZ-AU_ioL_uXv5Un0kNGPNhE&usqp=CAU',
+          'Anthony',
+          'What do you think?',
+          DateTime.now(),
+          2),
+    ];
+  }
+}
--- lib/view/chats_page.dart
+++ lib/view/chats_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:secure_chat/service/chat_service.dart';
 
 class ChatsPage extends StatefulWidget {
   const ChatsPage({super.key});
@@ -11,21 +11,69 @@ class ChatsPage extends StatefulWidget {
 }
 
 class _ChatsPageState extends State<ChatsPage> {
+  List<Chat> chats = [];
+  var chatService = ChatService();
+
+  @override
+  void initState() {
+    super.initState();
+
+    chats = chatService.getChats();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
       child: Column(children: [
         Expanded(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
-              'Chats',
-              style: TextStyle(fontSize: 20),
-            )
-          ],
-        )),
+          child: ListView.builder(
+            itemCount: chats.length,
+            shrinkWrap: true,
+            padding: const EdgeInsets.only(top: 10, bottom: 10),
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.fromLTRB(15, 20, 15, 0),
+                child: Row(children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(chats[index].avatarUrl),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Column(children: [
+                        Row(children: [Text(chats[index].name)]),
+                        Row(
+                          children: [Text(chats[index].lastMessageText)],
+                        )
+                      ]),
+                    ),
+                  ),
+                  Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+                    Row(children: [
+                      Text(chats[index].getLastMessageDateAsString()),
+                    ]),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 5),
+                      child: Container(
+                        width: 20,
+                        height: 20,
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            color: Colors.blue),
+                        child: Center(
+                            child: Text(
+                          chats[index].unread.toString(),
+                          style: const TextStyle(fontSize: 10),
+                        )),
+                      ),
+                    ),
+                  ])
+                ]),
+              );
+            },
+          ),
+        ),
       ]),
     ));
   }
