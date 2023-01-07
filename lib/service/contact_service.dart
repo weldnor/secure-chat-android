2e02ef484f3b806327cc6a48d7801ec26c10934c
Anton Kolesov <kolesov.anton.s@gmail.com>
2023-01-07 17:15:16 +0300

    Improve contacts page

---
+++ lib/service/contact_service.dart
@@ -0,0 +1,27 @@
+class Contact {
+  String avatarUrl;
+  String name;
+
+  Contact(this.avatarUrl, this.name);
+
+  @override
+  String toString() {
+    return 'Contact{avatarUrl: $avatarUrl, name: $name}';
+  }
+}
+
+abstract class AbstractContactService {
+  List<Contact> getContacts();
+}
+
+class ContactService implements AbstractContactService {
+  @override
+  List<Contact> getContacts() {
+    return [
+      Contact('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoqWIPKg9kRQhn9r3qgpcRSACAXvg-dbTOWQiDN6b5ahLRZ-AU_ioL_uXv5Un0kNGPNhE&usqp=CAU', 'Anthony'),
+      Contact('https://www.kindpng.com/picc/m/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png', 'Bryan'),
+      Contact('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSurwho7zsjHiiB2dUTQfuBJ075piTV0aYUQoB3jklw5_4nIiT8WceNEET6hg5YNElL8ks&usqp=CAU', 'Brianna'),
+      Contact('https://img.freepik.com/vector-premium/retrato-avatar-hombre-joven-caucasico-marco-redondo-ilustracion-plana-dibujos-animados-vector_551425-19.jpg?w=2000', 'Diana'),
+    ];
+  }
+}
--- lib/view/contacts_page.dart
+++ lib/view/contacts_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:secure_chat/service/contact_service.dart';
 
 import 'add_contact_page.dart';
 
@@ -12,21 +13,61 @@ class ContactsPage extends StatefulWidget {
 }
 
 class _ContactsPageState extends State<ContactsPage> {
+  var contactService = ContactService();
+  List<Contact> contacts = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    contacts = contactService.getContacts();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(children: [
-          Expanded(
-              child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
+          Row(children: const [
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
                 'Contacts',
-                style: TextStyle(fontSize: 20),
-              )
-            ],
-          )),
+                style: TextStyle(fontSize: 24),
+              ),
+            )
+          ]),
+          Expanded(
+            child: ListView.builder(
+              itemCount: contacts.length,
+              shrinkWrap: true,
+              padding: const EdgeInsets.only(top: 10, bottom: 10),
+              physics: const NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
+                      child: Row(children: [
+                        Column(
+                          children: [
+                            CircleAvatar(
+                                backgroundImage:
+                                    NetworkImage(contacts[index].avatarUrl)),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Text(contacts[index].name,
+                              style: const TextStyle(fontSize: 16)),
+                        )
+                      ]),
+                    )
+                  ],
+                );
+              },
+            ),
+          ),
         ]),
       ),
       floatingActionButton: FloatingActionButton(
